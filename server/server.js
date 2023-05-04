af13e9312f3410a87a5a0c5f7db6d8a2afe9629c
SJohnRose <shamini.johnrose@gmail.com>
2023-05-04 20:16:08 +1000

    created Teacher model and connection.js

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ app.get('/', (req, res) => {
 const startServer = async (typeDefs, resolvers) => {
     await server.start();
     server.applyMiddleware({ app });
-    
+    console.log("Hello");
     db.once('open', () => {
       app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
-        //console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
       })
     })
 };
