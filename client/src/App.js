e743a822e9bfa2e455f284f9420079c30401a5d6
SJohnRose <shamini.johnrose@gmail.com>
2023-05-06 15:35:34 +1000

    added Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import Home from './pages/Home';
+import Header from './components/Header';
+
 import './App.css';
-import Hello from './components/Hello';
+import './index.css';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <h1> Hello World!</h1> 
-      <Hello />
-    </div>
+    <ApolloProvider client={client}>
+      <div className = "main">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header">
+        <h1 style={{ fontSize: '3rem' }}>
+          My Rewards
+        </h1>
+        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+          Well done on your achievements!
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
