3a5cec545bd91afd8baa2289883faf2254bdccb2
SJohnRose <shamini.johnrose@gmail.com>
2023-05-14 21:48:59 +1000

    updated deletestudent styling

--- client/src/pages/DeleteStudent.js
+++ client/src/pages/DeleteStudent.js
@@ -38,18 +38,18 @@ export default function DeleteStudent(props) {
        
     
     return (
-      <div>
+      <div className="page">
              
         {data ? (
               <p>
-                Success! You may now head{' '}
+                Success! Student removed from Database.You may now head{' '}
                 <Link to="/student">back.</Link>
               </p>
          ) : (
               
-        <form className="login-form" onSubmit={handleFormSubmit}>
-          <h2 className="login-title">Add New Student </h2>
-          Enter Student Code:
+        <form className="entry-form" onSubmit={handleFormSubmit}>
+          <h2 className="entry-title">Remove Student </h2>
+          <h5>Enter Student Code to delete:</h5>
           <input
             value={studentCode}
             name="studentCode"
