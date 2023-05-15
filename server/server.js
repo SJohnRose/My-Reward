7d3d96aea15730f5d4847946d957ffad370a6629
SJohnRose <shamini.johnrose@gmail.com>
2023-05-15 21:11:21 +1000

    updated package.json with start script

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const server = new ApolloServer({
     typeDefs,
@@ -19,9 +19,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
+// }
   
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -33,10 +33,13 @@ const startServer = async (typeDefs, resolvers) => {
     console.log("Hello");
     db.once('open', () => {
       app.listen(PORT, () => {
+        console.log('Connected to MongoDB');
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
       })
-    })
+    });
+
+
 };
 
 startServer(typeDefs, resolvers);
