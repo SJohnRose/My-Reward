89f2bd9278d360a9edbe7987fdc1cddef90c004c
SJohnRose <shamini.johnrose@gmail.com>
2023-05-15 22:45:19 +1000

    updated

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// }
   
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
